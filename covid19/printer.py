0f4d7d61716f238f9ce24689dffd5bed4deb4b78
Ankush Patil <aspraz2658@gmail.com>
2020-04-05 11:38:49 +0530

    boring colouring on windows default white

--- covid19/printer.py
+++ covid19/printer.py
@@ -1,27 +1,42 @@
+import os
+import platform
+
+
 class Rangebi:
     # colour constants
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    # Issue#6 temparary printing in white for windows
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+    else:
+        HEADER = ''
+        OKBLUE = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
 
-    def get_in_danger(_self, message):
+    def get_in_danger(self, message):
         return Rangebi.FAIL + str(message) + Rangebi.ENDC
 
-    def get_in_warning(_self, message):
+    def get_in_warning(self, message):
         return Rangebi.WARNING + str(message) + Rangebi.ENDC
 
-    def get_in_bold(_self, message):
+    def get_in_bold(self, message):
         return Rangebi.BOLD + str(message) + Rangebi.ENDC
 
-    def get_in_success(_self, message):
+    def get_in_success(self, message):
         return Rangebi.OKGREEN + str(message) + Rangebi.ENDC
 
-    def get_in_info(_self, message):
+    def get_in_info(self, message):
         return Rangebi.OKBLUE + str(message) + Rangebi.ENDC
 
 
