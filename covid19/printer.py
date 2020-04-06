5998681f452f4f17bbeff6adc346243b1adb405d
Ankush <aspraz2658@gmail.com>
2020-04-06 18:19:24 +0530

    Colouring texts (#7)
    * Fixed windows styling using coloroma
    
    * Fixed typo

--- covid19/printer.py
+++ covid19/printer.py
@@ -1,28 +1,22 @@
 import os
 import platform
+import colorama
 
 
 class Rangebi:
     # colour constants
     # Issue#6 temparary printing in white for windows
-    if platform.system() == 'Linux' or platform.system() == 'Darwin':
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
-    else:
-        HEADER = ''
-        OKBLUE = ''
-        OKGREEN = ''
-        WARNING = ''
-        FAIL = ''
-        ENDC = ''
-        BOLD = ''
-        UNDERLINE = ''
+    if platform.system() == 'Windows':
+        colorama.init()
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
     def get_in_danger(self, message):
         return Rangebi.FAIL + str(message) + Rangebi.ENDC
